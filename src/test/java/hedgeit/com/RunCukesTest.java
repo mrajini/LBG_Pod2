139302397db95ea6bd4d79a222c0b7ee9ccb00a3
Rajini <rajini27426@gmail.com>
2018-06-11 12:16:05 +0100

    changes made to Runner class

--- src/test/java/hedgeit/com/RunCukesTest.java
+++ src/test/java/hedgeit/com/RunCukesTest.java
@@ -5,6 +5,14 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "io.cucumber.pro.JsonReporter:all"})
+@CucumberOptions(
+        features = {"/Users/Rajini1/Documents/LBGWorkspace/LBG_Pod2/src/test/resources/hedgeit/com/sample.feature"},
+        plugin = {"pretty","html:target/reportshtml","json:target/reportsjson"},
+        tags = {"@Rajini"
+
+}
+)
+
+
 public class RunCukesTest {
 }
