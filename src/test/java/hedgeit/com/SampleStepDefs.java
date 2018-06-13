cdf010d332cf81a1748e518e53c8d278661290c6
raviahujauk <ravi_ahuja@hotmail.com>
2018-06-13 11:20:22 +0100

    Changes made on 13 June

--- src/test/java/hedgeit/com/SampleStepDefs.java
+++ src/test/java/hedgeit/com/SampleStepDefs.java
@@ -4,7 +4,13 @@ import com.rest.hedgeit.HedgeItTradeGet;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.util.List;
+import cucumber.api.DataTable;
+import org.testng.Assert;
 
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import org.testng.Assert;
 
 
 public class SampleStepDefs {
@@ -29,8 +35,6 @@ public class SampleStepDefs {
         System.out.print("step three");
     }
 
-
-
     @Given("^An instance of Ergast motor racing season (\\d+)$")
     public void anInstanceOfErgastMotorRacingSeason(int int1) throws Throwable {
         Trade.racingSeason(int1);
@@ -38,22 +42,34 @@ public class SampleStepDefs {
 
     @When("^I request circuit data of season using the ReST API \"([^\"]*)\"$")
     public void iRequestCircuitDataOfSeasonUsingTheReSTAPI(String circuitAPI) throws Throwable {
-        //get(circuitAPI);
-        /* APIPath = circuitAPI;
-        get(APIPath).
-                then().
-                assertThat().
-                statusCode(200); */
         Trade.GetRequest(circuitAPI);
     }
 
     @Then("^I receive the JSON array of (\\d+) records$")
     public void iReceiveTheJSONArrayOfRecords(int int1) throws Throwable {
-         /* get(APIPath).
-                then().
-                assertThat().
-                body("MRData.CircuitTable.Circuits.circuitId", hasSize(20)); */
-
         Trade.assertRecords(int1);
     }
+
+    @Given("^an instance of HedgeIt that contains following trade (\\d+)$")
+    public void instanceOfHedgeItTrade(int arg1) throws Exception {
+        System.out.println(arg1);
+
+    }
+
+    @When("^I request that trade using following ReST API \"([^\"]*)\"$")
+    public void getTradeAPI(String API) throws Exception {
+        //System.out.println("API is : "+API);
+        int code = get(API).getStatusCode();
+        System.out.println(code);
+        Assert.assertEquals(code, 200);
+    }
+
+    @Then("^I receive the following JSON array with following:$")
+    public void i_receive_the_following_JSON_array_with_following(DataTable table) throws Exception {
+        List<List<String>> data = table.raw();
+        System.out.println(" "+data.get(0).get(1));
+        System.out.println(" "+data.get(1).get(1));
+
+    }
+
 }
--- src/test/resources/hedgeit/com/sample.feature
+++ src/test/resources/hedgeit/com/sample.feature
@@ -13,9 +13,14 @@ Feature: Belly
     Then I receive the JSON array of 20 records
 
   @FunctionalTest
-  Scenario:
-  Given an instance of HedgeIt that contains trade id 11
-  When I request that trade using the ReST API
-  http://<server>:16003/service/trades/8549748LO1/20180524
-  Then I receive the following JSON array
-  [{....}]```
+  Scenario Outline:
+    Given an instance of HedgeIt that contains following trade <id>
+    When I request that trade using following ReST API <rest_api>
+    Then I receive the following JSON array with following:
+    | Trade Name   | <trade_name> |
+    | Trade Mode   | <trade_mode> |
+    Examples:
+    | id            | rest_api                      | trade_name      | trade_mode |
+    | 11            | "http://localhost:3000/Trade"   | Hedgeit Bonds   | Cash       |
+    | 22            | "http://localhost:3000/Trade"   | Currency Funds  | SWIFT      |
+    | 33            | "http://localhost:3000/Trade   | Equity Funds    | Cash       |
